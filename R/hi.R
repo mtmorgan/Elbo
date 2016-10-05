759ae3a1cce7e546513cf6958f02923d79537b4e
Martin Morgan <martin.morgan@roswellpark.org>
2016-10-05 17:41:35 -0400

    implement 'how' for function hi()
    - 'shout' for ALL CAPS, 'whisper' for lower case

--- R/hi.R
+++ R/hi.R
@@ -6,21 +6,31 @@
 #' @param who character(1) The name of the person you wish to start a
 #'     conversation with.
 #'
+#' @param how character(1) How to greet the conversant. Either "shout" (default)
+#'     or "whisper".
+#'
 #' @return character(1) A line of text to be used when starting conversations.
 #'
 #' @examples
 #' hi("Martin Morgan")
+#' hi("Martin Morgan", "whisper")
 #'
 #' @export
-hi <- function(who) {
+hi <- function(who, how=c("shout", "whisper")) {
     stopifnot(
         is.character(who),
         length(who) == 1,
         !is.na(who)
     )
-    paste("hello", shout(who), "you have", nchar(who), "letters in your name!")
+    how <- match.arg(how)
+    fun <- switch(how, shout=shout, whisper=whisper)
+    paste("hello", fun(who),  "you have", nchar(who), "letters in your name!")
 }
 
 shout <- function(who) {
     toupper(who)
+}
+
+whisper <- function(who) {
+    tolower(who)
 }
