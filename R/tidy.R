77c6f885fae8481342207deaffa62d1e6a5a52d2
Martin Morgan <martin.morgan@roswellpark.org>
2016-10-07 17:53:41 -0400

    implement 'input_tidy'
    - represent data as long form tbl_df

---
+++ R/tidy.R
@@ -0,0 +1,37 @@
+#' Represent data in a 'tidy' format
+#' 
+#' 'Tidy' data takes a 'long' view of data, where each
+#' variable is a column.
+#' 
+#' @details This approach popularized by Hadley Wickham; see the
+#'    vignette in the package 'dplyr'
+#'    
+#' @param pdata_file character(1)  The path to the pheno data
+#'     file.
+#'     
+#' @param exprs_file character(1) The path to the expression
+#'     file.
+#'     
+#' @return A data.frame() with 'tidy' formatting.
+#'    
+#' @importFrom reshape2 melt
+#' @importFrom dplyr tbl_df
+#' @importFrom magrittr %>%
+#' 
+#' @export
+input_tidy <- function(pdata_file, exprs_file) {
+    classic <- input_classic(pdata_file, exprs_file)
+    melt(classic, id.vars=1:22, variable.name="probeset",
+         value.name="exprs") %>% tbl_df
+}
+
+
+
+
+
+
+
+
+
+
+
