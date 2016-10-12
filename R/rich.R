e5f000000b20f985aa2652dfa8ce57ff53246ab7
Martin Morgan <martin.morgan@roswellpark.org>
2016-10-12 16:41:20 -0400

    implement rich data (SummarizedExperiment) input

---
+++ R/rich.R
@@ -0,0 +1,26 @@
+#' Rich data input
+#' 
+#' Capture the semantic meaning of the data that
+#' we represent. For instance, separately
+#' recognizing the 'phenotypic' data (descrbing
+#' the samples) and the 'expression' values 
+#' (derived from, e.g., microarray or RNAseq
+#' experiment).
+#' 
+#' @param pdata_file character(1) The path to the pheno data
+#'     file.
+#'     
+#' @param exprs_file character(1) The path to the expression
+#'     file.
+#'     
+#' @return A SummarizedExperiment() containing samples as
+#'     colData(), and the expression values as assay().
+#' 
+#' @importFrom SummarizedExperiment SummarizedExperiment
+#' @export
+input_rich <- function(pdata_file, exprs_file) {
+    pdata <- read.csv(pdata_file, row.names=1, check.names=FALSE)
+    exprs <- read.csv(exprs_file, row.names=1, check.names=FALSE)
+    
+    SummarizedExperiment(as.matrix(exprs), colData=pdata)
+}
