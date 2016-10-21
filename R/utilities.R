3b0a0ccf396c0c26ab6e2b8e74ad12097b427c60
Martin Morgan <martin.morgan@roswellpark.org>
2016-10-21 16:42:54 -0400

    Add vignette

--- R/utilities.R
+++ R/utilities.R
@@ -1,3 +1,8 @@
+#' Test that argument is a scalar character
+#' 
+#' @param x character(1)
+#' 
+#' @return logical(1)
 is_scalar_character <- function(x) {
     is.character(x) && length(x) == 1 && 
         !is.na(x) && nzchar(x)
